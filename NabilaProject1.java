f352b69787b058f767f4d507893853f6d79af1dd
nanabilaa <putriiynabila@gmail.com>
2024-08-27 09:28:32 +0700

    RightFile

---
+++ NabilaProject1.java
@@ -0,0 +1,9 @@
+package com.mycompany.nabilaproject1;
+public class NabilaProject1 {
+    public static void main(String[] args) {
+        System.out.println("Nama : Nabila Kartika Putri");
+        System.out.println("NPM : 2320506039");
+        System.out.println("Alamat : Tuguran ");
+        System.out.println("NO_HP : 08820000865388");
+    }
+}
